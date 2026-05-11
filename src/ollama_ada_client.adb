54f48d4d3bfff288c6c8417b62bcbad4db106c40
Terry Matsufuji <terrymatsufuji@protonmail.com>
2026-05-11 13:01:18 +0900

    add using clause

--- src/ollama_ada_client.adb
+++ src/ollama_ada_client.adb
@@ -1,8 +1,8 @@
-with Ada.Text_IO;
+with Ada.Text_IO;    use Ada.Text_IO;
 
 procedure Ollama_Ada_Client is
 begin
    --  print hello world;
-   Ada.Text_IO.Put_Line ("Hello world");
+   Put_Line ("Hello world");
 
 end Ollama_Ada_Client;
